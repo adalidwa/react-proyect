4d26f8bfd8a790b7bae44ee607e7c02bfc6bae99
adalidwa <adalidjesusxd@gmail.com>
2024-04-16 22:21:26 -0400

    Esta vez muestra todos los personajes aleatoriamente del 1 al 826

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,34 @@ import Card from './componetes/Card';
 import './App.css';
 import { useEffect, useState } from 'react';
 
+
 function App() {
   const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
-    fetch("https://rickandmortyapi.com/api/character")
-      .then((res) => res.json())
-      .then((data) => {
-        // Obtener un índice aleatorio para seleccionar los personajes
-        const randomIndex = Math.floor(Math.random() * (data.results.length - 10)); // Asegura que al menos haya 10 personajes disponibles
-        // Tomar solo los siguientes 10 personajes comenzando desde el índice aleatorio
-        const selectedCharacters = data.results.slice(randomIndex, randomIndex + 10);
-        setCharacters(selectedCharacters);
+    // Función para generar 10 números aleatorios únicos entre 1 y 826
+    const generateRandomNumbers = () => {
+      const numbers = [];
+      while (numbers.length < 10) {
+        const randomNum = Math.floor(Math.random() * 826) + 1;
+        if (!numbers.includes(randomNum)) {
+          numbers.push(randomNum);
+        }
+      }
+      return numbers;
+    };
+
+    // Obtener los IDs aleatorios
+    const randomIds = generateRandomNumbers();
+
+    // Hacer solicitudes para obtener los personajes correspondientes a los IDs aleatorios
+    Promise.all(randomIds.map(id => fetch(`https://rickandmortyapi.com/api/character/${id}`)
+    .then(res => res.json())))
+      .then(data => {
+        setCharacters(data);
+      })
+      .catch(error => {
+        console.error('Error fetching characters:', error);
       });
   }, []);
 
