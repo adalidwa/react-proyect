cd21f6cca5b30c6e7e4eb2aa43fbcb1f1119ba9f
adalidwa <adalidjesusxd@gmail.com>
2024-04-19 07:42:41 -0400

    CharacterEpisode terminado

--- src/componetes/CharacterEpisode.jsx
+++ src/componetes/CharacterEpisode.jsx
@@ -8,20 +8,22 @@ function CharacterEpisode({ character }) {
     fetch("https://rickandmortyapi.com/api/episode")
       .then((res) => res.json())
       .then((data) => {
-        setEpisodes(data.results);
+        setEpisodes(data.results.slice(0, 5)); // Limitando a los primeros 5 episodios
       });
   }, []);
 
   return (
     <div className="Episodio">
-      <h2>Episodios:</h2>
-      {episodes.map((episode) => (
-        <div key={episode.id}>
-          <h4>{episode.name}</h4>
-          <p>Episodio: {episode.episode}</p>
-          <p>Air Date: {episode.air_date}</p>
-        </div>
-      ))}
+      <h2>Episodios</h2>
+      <div className="Episodios">
+        {episodes.map((episode) => (
+          <div key={episode.id}>
+            <h4>{episode.name}</h4>
+            <p>Episodio: {episode.episode}</p>
+            <p>Air Date: {episode.air_date}</p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
