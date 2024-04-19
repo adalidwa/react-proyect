d20bcef20bea060c558d043195494ed5df684c48
adalidwa <adalidjesusxd@gmail.com>
2024-04-19 11:52:57 -0400

    Iniciando la navegacion con React-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
-import CharacterCard from './componetes/CharacterCard';
+
 import './App.css';
-import { useEffect, useState } from 'react';
+import RickandMortyApp from './RickandMortyApp';
+
 
 
 function App() {
-  const [character, setCharacter] = useState();
-
-  useEffect(() => {
-    fetch("https://rickandmortyapi.com/api/character")
-      .then((res) => res.json())
-      .then((data) => setCharacter(data.results[0])); // Solo almacenar el primer personaje
-  }, []);
 
   return (
-    <div>
-      {character && <CharacterCard character={character} />}
-    </div>
+    <RickandMortyApp/>
   );
 }
 
---
+++ src/RickandMortyApp.jsx
@@ -0,0 +1,22 @@
+import CharacterCard from './componetes/CharacterCard';
+import './App.css';
+import { useEffect, useState } from 'react';
+
+
+function RickandMortyApp() {
+  const [character, setCharacter] = useState();
+
+  useEffect(() => {
+    fetch("https://rickandmortyapi.com/api/character")
+      .then((res) => res.json())
+      .then((data) => setCharacter(data.results[0])); // Solo almacenar el primer personaje
+  }, []);
+
+  return (
+    <div>
+      {character && <CharacterCard character={character} />}
+    </div>
+  );
+}
+
+export default RickandMortyApp;
