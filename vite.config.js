64c4fcf54e2f68c2ce130e920b6fb7b430402bb4
adalidwa <adalidjesusxd@gmail.com>
2024-04-25 10:22:55 -0400

    fata

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  /* base: "https://adalidwa.github.io/react-proyect/", */
+
   plugins: [react()],
-  base: "https://adalidwa.github.io/social-links-profile/",
 });
